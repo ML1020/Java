33fb3d3ca73751be542bbad50372e9f381cb0e12
ML1020 <1227797704@qq.com>
2020-02-19 23:06:03 +0800

    Super方法举例

---
+++ SuperDemo.java
@@ -0,0 +1,34 @@
+//package Demo;
+////
+////class Person{
+////    public void print(){
+////        System.out.println("1.I am father");
+////    }
+////}
+////class Student extends Person{
+////    public void print(){
+////        super.print();
+////        System.out.println("2.I am child");
+////    }
+////}
+////public class SuperDemo{
+////    public static void main(String[] args) {
+////        new Student().print();
+////    }
+////}
+//class Person{
+//    public String info = "爸爸！ ";
+//}
+//class Student extends Person{
+//    public String info = "⼉⼦！ " ;
+//    public void print(){
+////不找本类中的属性
+//        System.out.println(super.info);
+//        System.out.println(this.info);
+//    }
+//}
+//public class SuperDemo{
+//    public static void main(String[] args) {
+//        new Student().print();
+//    }
+//}
