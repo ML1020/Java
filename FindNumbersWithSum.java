f1bcce902d12eb949adfe3929df5d13cce6c9111
ML1020 <1227797704@qq.com>
2020-09-07 22:42:18 +0800

    找到和为sum的两个数，若有多个，则找乘积最小的

--- FindNumbersWithSum.java
+++ FindNumbersWithSum.java
@@ -3,6 +3,8 @@ package Solution;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+//输入一个递增排序的数组和一个数字S，在数组中查找两个数，
+// 使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
 public class FindNumbersWithSum{
     //暴力解法
     public ArrayList<Integer> findNumbersWithSum(int [] array,int sum) {
