c179ec0a34ea063fbeb62b4bf675efa45f3fdaa4
ML1020 <1227797704@qq.com>
2020-09-10 23:02:31 +0800

    旋转后求最小数

--- MinNumberInRotateArray.java
+++ MinNumberInRotateArray.java
@@ -1,11 +1,7 @@
-package Important;
+package Solution;
 
+import java.util.ArrayList;
 public class MinNumberInRotateArray {
-    public static void main(String[] args) {
-        int[] array ={1,0,1,1,1};
-//        int[] array ={1,1,1,0,1};
-        minNumberInRotateArray(array);
-    }
     public static int minNumberInRotateArray(int[] array) {
         int left = 0;
         int right = array.length - 1;
@@ -15,7 +11,6 @@ public class MinNumberInRotateArray {
             int mid = left + (right - left) / 2;
             //如果最左边的数小于最右边的数，则此数组是递增数列，返回左边最小的
             if (array[left] < array[right]) {
-                System.out.println(array[left]);
                 return array[left];
             }
             if (array[left] < array[mid]) {
@@ -28,5 +23,14 @@ public class MinNumberInRotateArray {
         }
         System.out.println(array[left]);
         return array[left];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+        System.out.println(minNumberInRotateArray(arr));
+        ;
+//        for (int i = 0;i < arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
     }
 }
